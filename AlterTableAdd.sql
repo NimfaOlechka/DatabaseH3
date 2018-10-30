a8c6708d88bae504d2d909dab46bf5b633579f62
Olga K <nimfaolya@mail.ru>
2018-10-30 14:56:56 +0100

    alter sql

--- AlterTableAdd.sql
+++ AlterTableAdd.sql
@@ -1,1 +1,3 @@
-ALTER TABLE Varer ADD ShipperID int ;
+--ALTER TABLE Varer ADD ShipperID int ;
+--ALTER TABLE Ansatte ADD Filial int;
+ALTER TABLE Ansatte ADD FOREIGN KEY (Filial) REFERENCES Filialer(FilialID)
