ab9ff58a70eb466da874667948ed280e59d1cf5f
Olga K <nimfaolya@mail.ru>
2018-10-31 14:58:54 +0100

    alter quieries

--- AlterTableAdd.sql
+++ AlterTableAdd.sql
@@ -1,3 +1,4 @@
 --ALTER TABLE Varer ADD ShipperID int ;
 --ALTER TABLE Ansatte ADD Filial int;
-ALTER TABLE Ansatte ADD FOREIGN KEY (Filial) REFERENCES Filialer(FilialID)
+--ALTER TABLE Ansatte ADD FOREIGN KEY (Filial) REFERENCES Filialer(FilialID)
+ALTER TABLE Varer ADD Price money;
