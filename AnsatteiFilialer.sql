1983a79665889d8c5f06cdf13d85830c63809d4e
Olga K <nimfaolya@mail.ru>
2018-10-31 14:59:53 +0100

    ansatte i filialer

--- AnsatteiFilialer.sql
+++ AnsatteiFilialer.sql
@@ -1,4 +1,4 @@
-SELECT Ansatte.Navn, Filialer.FilialNavn, Filialer.FilialBy 
+SELECT Ansatte.Navn + ' '+ Ansatte.Efternavn as Ansatte, Filialer.FilialNavn + ' i ' + Filialer.FilialBy AS Filial
 FROM Filialer
 INNER JOIN Ansatte ON Ansatte.Filial = Filialer.FilialID
 ORDER BY FilialBy ASC;
